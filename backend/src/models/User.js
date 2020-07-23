02064526b95e3160286ab5e1e790fe47f0a3afeb
AloisioFernandes <68351209+AloisioFernandes@users.noreply.github.com>
2020-07-23 02:53:57 -0300

    removido

--- backend/src/models/User.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-//schema representa a estrutura, os campos que o usuário terá
-const UserSchema = new mongoose.Schema({
-    email: String,
-})
-
-module.exports = mongoose.model('User', UserSchema) //criando um usuário no banco de dados
